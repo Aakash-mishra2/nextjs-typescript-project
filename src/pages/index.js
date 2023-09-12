51f6857a8053a1cdca9590cdf07ffe87ff6a10b0
Aakash Mishra <aakashmishra.281@gmail.com>
2023-09-12 12:15:05 +0530

    first url routes

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+const myapp = () => {
+    return <h1>This is new project. </h1>
+}
+
+export default myapp;
---
+++ src/pages/next/index.js
@@ -0,0 +1,5 @@
+const newpage = () => {
+    return <h1>This is new page. </h1>
+}
+
+export default newpage;
